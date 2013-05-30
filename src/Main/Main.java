4279b384343fa99ec180c2a2c806c9dba6beecd6
zeus <zeus@Zeus15>
2013-05-30 12:01:49 -0500

    Existe un problema en el condicional de los showSet, está omitiendo el 63

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,5 @@
 package Main;
 
-
 import java.util.LinkedList;
 
 /*
@@ -17,11 +16,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n =5;
+        int n = 5;
 //        int n =Integer.parseInt(args[0]);
         LinkedList<Integer> a = new LinkedList<>();
         LinkedList<Integer> b = new LinkedList<>();
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 0; i <= 100; i++) {
             a.add(i);
             b.add(i + 100);
         }
@@ -37,7 +36,7 @@ public class Main {
 //        b.add(10);
 
 
-//        System.out.println(Set.isEqual(a, b));
+//        System.out.println(Set.isEqual(b, b));
 //        System.out.println(Set.isEmpty(a));
 //        System.out.println(Set.isSubSet(a, b));
 //        System.out.println(Set.isDisjunct(a, b));
@@ -46,11 +45,11 @@ public class Main {
 //        System.out.println(Set.diference(a, b));
 
 
-        int a2[] = new int [100];
-        int b2[] = new int [100];
+        int a2[] = new int[100];
+        int b2[] = new int[100];
         for (int i = 0; i < 100; i++) {
-            a2[i]= i +1;
-            b2[i]= i + 101;
+            a2[i] = i;
+            b2[i] = i + 100;
         }
 
 //        System.out.println(Set.isEqual(a2, b2));
@@ -59,11 +58,16 @@ public class Main {
 //        System.out.println(Set.isDisjunct(a2, b2));
 //        System.out.println(Set.showSet(Set.intersect(a2, b2)));
 //        System.out.println(Set.showSet(Set.junction(a2, b2)));
+        System.out.println("Conjuntos De enteros");
+        System.out.println(Set.showSet(a2));
+        
 //        System.out.println(Set.diference(a2, b2));
 
 
-        long a3[] = {Long.MAX_VALUE,68719476735l, 0, 0};
-        long b3[] = {0, 68719476735l, 0, 0};
+              
+        
+        long a3[] = {Long.MAX_VALUE, 68719476735l, 0, 0};
+        long b3[] = {0, 0, Long.MAX_VALUE, 68719476735l};
 //        System.out.println(Set.isEqual(a3, b3));
 //        System.out.println(Set.isEmpty(a2));
 //        System.out.println(Set.isSubSet(a2, b2));
@@ -71,6 +75,10 @@ public class Main {
 //        System.out.println(Set.showSet(Set.intersect(a3, b3)));
 //        System.out.println(Set.showSet(Set.junction(a3, b3)));
 //        System.out.println(Set.showSet(Set.diference(a3, b3)));
+        System.out.println("Conjuntos de LONG");
+//        System.out.println(Set.showSet(a3));
+//        System.out.println(Set.showSet(b3));
+        System.out.println((Long.MAX_VALUE & 1l << 63));
         System.out.println(Set.showSet(a3));
 //        System.out.println(Long.toBinaryString((long)1 << 62));
     }
